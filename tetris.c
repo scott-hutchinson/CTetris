e0e5e636f9fab8bcd3ec5797c21916bbf3cc18f1
Scott Hutchinson <liteplate@gmail.com>
2014-02-18 06:25:48 -0600

    Removed and refactored old code

--- tetris.c
+++ tetris.c
@@ -4,9 +4,32 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "renderer.h"
 #include "terminal.h"
 #include "input.h"
 
+
+struct Tetris {
+    Renderer *renderer;
+
+    Block *current_block, *ghost_block;
+
+    GAME_STATE game_state;
+
+    uint32_t score, level, lines_until_level_up, lines_completed;
+
+    uint8_t enable_ghost_block;
+
+    uint8_t gravity_frame_counter, movement_frame_counter;
+    uint8_t gravity_frame_delay, movement_frame_delay;
+
+    int current_key, current_key_sequence[3];
+
+    unsigned char key_left, key_right, key_down, key_drop, key_rotate, key_quit, key_pause, key_ghost_block;
+
+    int option_count;
+    char **options;
+};
 
 static void init_screen(void)
 {
