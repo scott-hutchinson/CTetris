853710f4bd1edaa148d6b225d16c5c995aa92928
Scott Hutchinson <liteplate@gmail.com>
2015-01-03 12:08:32 -0600

    IXON should be disabled to prevent pausing gameplay

--- terminal.c
+++ terminal.c
@@ -14,6 +14,7 @@ void Terminal_begin_raw_mode(void)
     raw_term_attr = orig_term_attr;
 
     raw_term_attr.c_lflag &= (unsigned int) ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw_term_attr.c_iflag &= (unsigned int) ~(IXON);
     raw_term_attr.c_cc[VTIME] = 0;
     raw_term_attr.c_cc[VMIN] = 0;
 
